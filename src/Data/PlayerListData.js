ed6f00ad02f668197fdf3b24f2fa346fc9de67ac
Kachii <Kacheokcheong@gmail.com>
2021-05-21 17:50:16 +0100

    added site fetcher

--- src/Data/PlayerListData.js
+++ src/Data/PlayerListData.js
@@ -1,19 +1,19 @@
 const PlayerListData = [
     {
         "player_name": "Kach",
-        "points_total": 0,
-        "transfers_total": 0,
-        "current_gameweek": 0,
+        "points_total": "",
+        "transfers_total": "",
+        "current_gameweek": "",
         "matches": [
             {
-                "player_id": 0,
-                "gameweek": 0,
-                "game_week_points": 0,
-                "points_total": 0,
-                "team_value": 0,
-                "game_week_transfers": 0,
-                "game_week_transfers_cost": 0,
-                "bench_points": 0
+                "player_id": "",
+                "gameweek": "",
+                "game_week_points": "",
+                "points_total": "",
+                "team_value": "",
+                "game_week_transfers": "",
+                "game_week_transfers_cost": "",
+                "bench_points": ""
             },
         ]
     }
